6d6b45a47f81e0fcbe31264d6ed047357c704583
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-02-06 15:43:16 +0100

    MongoDb HookUp

--- frontend/lib/mongodb.ts
+++ frontend/lib/mongodb.ts
@@ -5,7 +5,7 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI
-const options = {}
+const options = {useUnifiedTopology: true}
 
 let client
 let clientPromise: Promise<MongoClient>
