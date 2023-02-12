3c5150cbdaf2c7c76387eeb73111fde43bdd96bb
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-02-13 00:40:20 +0100

    Update api's

--- frontend/pages/api/post/updatePost.ts
+++ frontend/pages/api/post/updatePost.ts
@@ -7,16 +7,19 @@ export default async (req:NextApiRequest, res:NextApiResponse) => {
    try {
     const client = await clientPromise;
     const db = client.db("App_Db");
-    const id = (req.body.epost)
+    const id = (req.body.titel)
     console.log(id)
     const updateDocument={
         $set:{
-            name:req.body.name,
-            password:req.body.password
+            titel:req.body.titel,
+            content:req.body.content,
+            date:req.body.date,
+            dislikes:req.body.dislikes,
+            likes:req.body.likes
 
         },
     };
-    const result = await db.collection("users").updateOne({epost:req.body.epost},updateDocument)
+    const result = await db.collection("posts").updateOne({titel:req.body.titel},updateDocument)
     res.status(200).json(updateDocument+id)
 
     
