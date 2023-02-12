486dbfd6a099912155d91fe16bf54393e0f46231
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-02-12 22:42:32 +0100

    PRogression

--- frontend/pages/api/user/updateUser.ts
+++ frontend/pages/api/user/updateUser.ts
@@ -2,15 +2,29 @@ import clientPromise from "../../../lib/mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ObjectId } from "mongodb";
 
+
 export default async (req:NextApiRequest, res:NextApiResponse) => {
    try {
-       const client = await clientPromise;
-       const db = client.db("App_Db");
-       const id = req?.body.id;
-       const query = { _id: new ObjectId(id) };
-       let bodyObject = JSON.parse(req.body);
-       let myPost = await db.collection("posts").updateOne(query,bodyObject);
-       res.json(req);
+    const client = await clientPromise;
+    const db = client.db("App_Db");
+    const id = (req.body._id)
+    console.log(id)
+
+    const filter = new ObjectId(id)
+    const updateDocument={
+        $set:{
+            titel:req.body.titel,
+            content:req.body.content,
+            date:req.body.date,
+            dislikes:req.body.dislikes,
+            likes:req.body.likes
+
+        },
+    };
+    const result = await db.collection("posts").updateOne({_id:filter},updateDocument)
+    res.status(200).json(updateDocument+id)
+
+    
    } catch (e) {
        console.error(e);
    }
