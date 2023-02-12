f4f3aff3e4f249dadac0a7b413ac7f57bb17bd97
johlil9 <243333@usn.no>
2023-02-12 19:51:13 +0100

    Nav header og footer utkast.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '@/components/header/Header'
+import Footer from '@/components/header/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+          <Header/> 
+          <Component {...pageProps} />
+          
+          <Footer/>
+          </>
+           )
 }
