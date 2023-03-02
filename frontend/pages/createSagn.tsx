2a603788e0ed1fbd26f73b1b3ca140b8a4778787
Olav <paalerud@hotmail.com>
2023-03-02 14:25:36 +0100

    cs

--- frontend/pages/createSagn.tsx
+++ frontend/pages/createSagn.tsx
@@ -4,8 +4,8 @@ import NewSagnForm from "@/components/NewSagnForm"
 const createSagn = () =>{
     return (
         <>
-        <div id="editor">
-            <div className="w-full max-w-lg p-2">
+        <div id="editor" className="mt-5">
+            <div className="w-full max-w-lg p-2 m-auto shadow-md">
                 <div className="">
                     <NewSagnForm className="flex flex-col"/>
                 </div>
