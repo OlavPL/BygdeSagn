9db5ac9499676b904754985635277f2fb1f9b3de
Olav <paalerud@hotmail.com>
2023-03-13 16:43:13 +0100

    Update for build

--- frontend/components/scrapped_components/card_fortelling.tsx
+++ frontend/components/scrapped_components/card_fortelling.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
-import CardTags from "./cardTags"
-import LikeDislikeButtons from "./likeDislikeButtons"
+import CardTags from "../sagnCard/cardTags"
+import LikeDislikeButtons from "../sagnCard/likeDislikeButtons"
 
 interface Props {
     title: String,
