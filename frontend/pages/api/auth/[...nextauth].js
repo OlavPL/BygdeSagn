75a6998d1e986cb674020280ad2e49f910d27834
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-03-21 15:52:05 +0100

    simplecrypto,user auth mongo login

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -1,8 +1,12 @@
 import NextAuth from "next-auth/next"
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from 'next-auth/providers/google'
-import CredentialsProvider from "next-auth/providers/credentials"
+import clientPromise from "@/lib/mongodb";
+import SimpleCrypto from "simple-crypto-js"
+const SECRET_KEY= "my-secret-key"
+
 export default NextAuth({
+  
     providers:[
         
     GoogleProvider({
@@ -10,6 +14,7 @@ export default NextAuth({
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     CredentialsProvider({
+      
         // The name to display on the sign in form (e.g. "Sign in with...")
         name: "Credentials",
         // `credentials` is used to generate a form on the sign in page.
@@ -17,20 +22,33 @@ export default NextAuth({
         // e.g. domain, username, password, 2FA token, etc.
         // You can pass any HTML attribute to the <input> tag through the object.
         credentials: {
-          username: { label: "Username", type: "text", placeholder: "jsmith" },
+          email: { label: "Email", type: "text", placeholder: "jsmith" },
           password: { label: "Password", type: "password" }
         },
         async authorize(credentials, req) {
-          // Add logic here to look up the user from the credentials supplied
-          const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-    
+          if(!credentials?.password||!credentials?.email){
+
+            throw new Error('Email and password required')
+
+          }
+          const client = await clientPromise;
+          const db = client.db("App_Db");
+          
+          const user = await db.collection("users").findOne({email:credentials.email})
           if (user) {
-            // Any object returned will be saved in `user` property of the JWT
-            return user
+            const sc = new SimpleCrypto(SECRET_KEY);
+            const decryptedPassword = sc.decrypt(user.password);
+             /*console.log("Credentials passord: "+credentials.password)
+            console.log("db passord: "+ user.password)
+            console.log("decryptert db passord: " +decryptedPassword) */
+           if(credentials.password == decryptedPassword){
+            console.log("User logged inn")
+              return user
+           }
           } else {
-            // If you return null then an error will be displayed advising the user to check their details.
+            console.log("User failed")
             return null
-    
+            
             // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
           }
         }
@@ -41,7 +59,7 @@ export default NextAuth({
             return "/#"
         }
     },
-    secret:process.JWT_SECRET
+    secret:process.env.JWT_SECRET
 })
 
 
