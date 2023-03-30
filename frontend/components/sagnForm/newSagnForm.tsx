4b1dca73ce8d78c9280e0235f1e9c170da5c450e
Olav <paalerud@hotmail.com>
2023-03-30 20:01:27 +0200

    Url hotfix

--- frontend/components/sagnForm/newSagnForm.tsx
+++ frontend/components/sagnForm/newSagnForm.tsx
@@ -138,9 +138,9 @@ const postSagn = async (data:Inputs, router: NextRouter ) =>{
     body:JSON.stringify(JSOndata),
   }
   // console.log(JSOndata)
-  const endpoint=("https://bop3000-app.vercel.app/post/postPost")
-  // const response = await fetch(endpoint,options).catch()
-  // const result = response.json;
+  const endpoint=("https://bop3000-app.vercel.app/api/post/postPost")
+  const response = await fetch(endpoint,options).catch()
+  const result = response.json;
   
   toast.success("Sagn publisert", {
     position: "top-center",
