4d6c5857513a8e9d4aeeba684c37480eb46c8b9f
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-03-30 18:58:57 +0200

    Button sagn card fix, get single post api
    Button sagn card fix, get single post api

--- frontend/pages/api/post/getPost.ts
+++ frontend/pages/api/post/getPost.ts
@@ -4,10 +4,10 @@ export default async (req:NextApiRequest, res:NextApiResponse) => {
    try {
        const client = await clientPromise;
        const db = client.db("App_Db");
-       const postId= req.body.post_id;
+       const post_Id= req.body.post_id;
        const search = await db
            .collection("users")
-           .find({postId})
+           .find({post_Id})
            .sort({ metacritic: -1 })
            .toArray();
        res.status(200).json(search);
