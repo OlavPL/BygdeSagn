fa28d90fdf3c064520919f0b83729e0d22fdd969
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-04-03 10:49:58 +0200

    Next - auth login page

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -7,6 +7,12 @@ const SECRET_KEY= "my-secret-key"
 
 export default NextAuth({
   
+    pages: {
+        signIn: '../../login',
+        signOut: '/auth/signout',
+        error: '/auth/error', // Error code passed in query string as ?error=
+        newUser: '../../register' // New users will be directed here on first sign in (leave the property out if not of interest)
+      },
     providers:[
         
     GoogleProvider({
@@ -14,13 +20,7 @@ export default NextAuth({
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     CredentialsProvider({
-      
-        // The name to display on the sign in form (e.g. "Sign in with...")
-        name: "Credentials",
-        // `credentials` is used to generate a form on the sign in page.
-        // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-        // e.g. domain, username, password, 2FA token, etc.
-        // You can pass any HTML attribute to the <input> tag through the object.
+        name: "BygdeSagn Konto",
         credentials: {
           email: { label: "Email", type: "text", placeholder: "jsmith" },
           password: { label: "Password", type: "password" }
@@ -57,7 +57,7 @@ export default NextAuth({
     callbacks:{
         async redirect(url, baseUrl){
             return "/#"
-        }
+        },
     },
     secret:process.env.JWT_SECRET
 })
--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,105 +1,37 @@
-import { useState, ChangeEvent } from 'react';
-import Link from 'next/link';
-import React from 'react'
-import {useSession, signIn, signOut,getSession} from 'next-auth/react'
-import {NextRouter, useRouter } from "next/router"
+import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { getProviders, signIn } from "next-auth/react"
+import { getServerSession } from "next-auth/next"
+import  authOptions  from "./api/auth/[...nextauth]";
 
-const Login =(router: NextRouter)=> {
- //const{data}= useSession({required:true});
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const handleLogin = () => {
-    
-  }
-
-  const handleGoogleLogin = () => {
-    signIn('GoogleProvider')
-  }
-
-
-  const {data: session,status} = useSession()
-
-  if(status==='authenticated') {
-    
-   
-  }
-  else {
-    return (
-      <div className="flex flex-col items-center mt-20 min-h-screen">
-        <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-          <h1 className="text-2xl font-bold mb-4">Login</h1>
-          <form>
-            <div className="mb-4">
-              <label htmlFor="username" className="block text-gray-700 font-bold mb-2 w-32">
-                Username
-              </label>
-              <input
-                id="username"
-                type="text"
-                placeholder="Enter your username"
-                value={username}
-                onChange={handleUsernameChange}
-                className="w-full p-2 border border-gray-400 rounded-md"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block text-gray-700 font-bold mb-2 w-32">
-                Password
-              </label>
-              <input
-                id="password"
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={handlePasswordChange}
-                className="w-full p-2 border border-gray-400 rounded-md"
-              />
-            </div>
-            <div className="flex justify-between items-center">
-              <button type="button" onClick={handleLogin} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-32">
-                Login
-              </button>
-              <p className="text-sm text-gray-500">Har du ikke bruker? <Link href="/register" className ="text-blue-500 hover:underline cursor-pointer">Registrer her</Link></p>
-            </div>
-            <div className="my-4">
-              <button onClick={handleGoogleLogin} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-full">
-                <span className="flex items-center justify-center">
-                  <img src="/google-icon.svg" alt="Google icon" className="w-4 h-4 mr-2" />
-                  Sign in with Google
-                </span>
-              </button>
-            </div>
-          </form>
-        </div>
+export default function SignIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className="flex flex-col items-center mt-20 min-h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button onClick={() => signIn(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
       </div>
-    )
-  }
-  
+    </div>
+  )
 }
 
-export default Login;
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  
+  // If the user is already logged in, redirect.
+  // Note: Make sure not to redirect to the same page
+  // To avoid an infinite loop!
+  if (session) {
+    return { redirect: { destination: "/" } };
+  }
 
-/*
-export const getServerSideProps = async (context:Context)=>{
-    const session = await getSession(context);
-
-    if(!session){
-        return{
-            redirect:{
-               destination: '/login'
-            }
-        }
-    }
-    return{
-        props:{session},
-    }
-} */
+  const providers = await getProviders();
+  
+  return {
+    props: { providers: providers ?? [] },
+  }
+}
