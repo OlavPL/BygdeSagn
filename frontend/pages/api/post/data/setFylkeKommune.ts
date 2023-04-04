4b21713324e9568a08e114e797d2c4a30193ebaa
Olav <paalerud@hotmail.com>
2023-04-04 14:55:47 +0200

    Append

--- frontend/pages/api/post/data/setFylkeKommune.ts
+++ frontend/pages/api/post/data/setFylkeKommune.ts
@@ -16,32 +16,8 @@ export default async (req:NextApiRequest, res:NextApiResponse) => {
         const client = await clientPromise;
         const db = client.db("App_Db");
 
-        // const fylkeData = { 
-        //     _id:req.body._id,
-        //     fylkenavn:req.body.fylkenavn,
-        //     fylkesnummer:req.body.fylkesnummer,
-        //     kommuner:req.body.kommuner,
-        // } 
-
-        // const options:RequestInit={
-        // headers:{
-        //     'Content-Type':'application/json',
-        // },
-        // method:'POST',
-        // body:JSON.stringify(fylkeData),
-        // }
-        // let body = JSON.stringify(fylkeData);
-
-        // const response = await fetch("/api/user/registerUser",options).catch()
-
         let myPost = db.collection("Fylker3").insertMany(req.body);
         res.status(200).json(myPost);
-
-       //let myPost = await db.collection("posts").insertOne(bodyObject);
-      // let myPost = await db.collection("Fylker2").insertMany(bodyObject);
-       //metod to create index's
-      // db.collection("fylker").createIndex({"sted":1},{unique:true})
-      // res.status(200).json(myPost);
        console.log("Sted Created")
        
    } catch (e) {
