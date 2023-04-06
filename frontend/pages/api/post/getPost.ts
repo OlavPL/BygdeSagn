6968aedd5d05bb0d4b76706ac36f42c1e7b9b3ca
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-04-06 21:21:58 +0200

    Update getPost.ts

--- frontend/pages/api/post/getPost.ts
+++ frontend/pages/api/post/getPost.ts
@@ -6,10 +6,10 @@ export default async (req:NextApiRequest, res:NextApiResponse) => {
    try {
        const client = await clientPromise;
        const db = client.db("App_Db");
-       const post_Id= req.body.post_id;
+       const post_Id= req.body.postId;
        const search = await db
            .collection(process.env.POST_COLLECTION!)
-           .find({post_Id})
+           .find({postId})
            .sort({ metacritic: -1 })
            .toArray();
        res.status(200).json(search);
