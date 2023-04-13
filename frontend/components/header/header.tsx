e12290e259b7b0d30b9dd43ecf029a23e45fbe0f
johlil9 <243333@usn.no>
2023-04-13 18:56:01 +0200

    Changed styling and responsive header

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 
 import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
-import { faPen, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket, faKey, faPen, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
 import { AppContext } from '@/pages/_app';
 import {useSession,signOut,getSession} from 'next-auth/react'
 import Image from 'next/image';
@@ -65,8 +65,8 @@ const Header = () => {
         {/* Create Sagn Button */}
           <div className="flex items-center space-x-8 w-60 justify-end">
             <Link href={"/createSagn"}>
-              <button className="flex items-center space-x-1 font-medium text-textColor hover:text-secondary-800 focus:outline-none bg-primary-200 hover:bg-primary-700 rounded-md px-4 py-1.5">
-                <FontAwesomeIcon icon={faPen} className="text-2xl w-6 h-6 cursor-pointer transition-colors duration-200 ease-in-out fa-lg" />
+              <button className="flex items-center hidden md:block space-x-1 font-medium text-textColor hover:text-secondary-800 focus:outline-none bg-primary-200 hover:bg-primary-700 rounded-md px-4 py-1.5">
+                <FontAwesomeIcon icon={faPen} className="text-2xl w-6 h-6 cursor-pointer transition-colors duration-100 ease-in-out fa-lg" />
                 <span className="text-lg underline">Nytt Sagn</span>
               </button>
             </Link>
@@ -81,24 +81,40 @@ const Header = () => {
             role="menu"
             aria-labelledby="menu-button">
               <div className="padd py-2" role="none" >
-                <Link href="/profilePage" className="px-2 py-2 text-sm block" role="menuitem" id="menu-item-profile">
+                {/* <Link href="/profilePage" className="px-2 py-2 text-sm block" role="menuitem" id="menu-item-profile">
                   Min Profil
                 </Link>
                 <Link href="profilePageNew" className="px-2 py-2 text-sm block" role="menuitem" id="menu-item-2">
                   Faktisk profil side
+                </Link> */}
+                <Link href={"profilePage"} className="block hover:bg-primary-200">
+                <button className="flex items-center space-x-1 font-medium text-textColor focus:outline-none rounded-md px-4 py-1.5">
+                  <FontAwesomeIcon icon={faUser} className="text-2xl w-6 h-6 cursor-pointer ease-in-out fa-lg" />
+                  <span className="text-lg underline">Profil</span>
+                </button>
+                </Link>
+                <Link href={"createSagn"} className = "block md:hidden hover:bg-primary-200">
+                <button className="flex items-center space-x-1 font-medium  focus:outline-none rounded-md px-4 py-1.5">
+                  <FontAwesomeIcon icon={faPen} className="text-2xl w-6 h-6 cursor-pointer ease-in-out fa-lg" />
+                  <span className="text-lg underline">Nytt Sagn</span>
+                </button>
                 </Link>
                 {session ? (
                   <button
                     onClick={handleLogout}
-                    className="px-2 py-2 text-sm block text-black hover:text-blue-250"
+                    className="flex items-center space-x-1 font-medium hover:bg-red-400 w-full focus:outline-none rounded-md px-4 py-1.5"
                     role="menuitem"
                     id="menu-item-logout"
                   >
-                    Logout
+                    <FontAwesomeIcon icon={faArrowRightFromBracket} className="text-2xl w-6 h-6 cursor-pointer transition-colors ease-in-out fa-lg" />
+                    <span className="text-lg underline">Logout</span>
                   </button>
                 ) : (
-                  <Link href="/login" className="px-2 py-2 text-sm block text-black hover:text-blue-250" role="menuitem" id="menu-item-login">
-                    Login
+                  <Link href="/login" className="block hover:bg-primary-200" role="menuitem" id="menu-item-login">
+                    <button className="flex items-center space-x-1 font-medium focus:outline-none rounded-md px-4 py-1.5">
+                      <FontAwesomeIcon icon={faRightFromBracket} className="text-2xl w-6 h-6 cursor-pointer transition-colors ease-in-out fa-lg" />
+                      <span className="text-lg underline">Login</span>
+                    </button>
                   </Link>
                 )}
               </div>
