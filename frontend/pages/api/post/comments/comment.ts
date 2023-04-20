f4ce5810155ad0d966895493f1d29a96431e726c
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-04-20 20:11:39 +0200

    Comment api fix

--- frontend/pages/api/post/comments/comment.ts
+++ frontend/pages/api/post/comments/comment.ts
@@ -5,26 +5,26 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === "POST") {
     const client = await clientPromise;
     const db = client.db("App_Db");
-    const id = req.body.postId;
+    const id = req.body._id;
     const newComment = req.body.comment;
     const updateDocument = {
       $push: {
         comments: newComment
       },
     };
-    const result = await db.collection(process.env.POST_COLLECTION!).updateOne({ postId: id }, updateDocument);
+    const result = await db.collection(process.env.POST_COLLECTION!).updateOne({ _id: id }, updateDocument);
     res.status(200).json("Comments Updated" + " postId:" + id);
   } else if (req.method === "DELETE") {
     const client = await clientPromise;
     const db = client.db("App_Db");
-    const id = req.body.postId;
+    const id = req.body._id;
     const commentId = req.body.commentId;
     const updateDocument = {
       $pull: {
         comments: [{ id: commentId }]
       },
     };
-    const result = await db.collection(process.env.POST_COLLECTION!).updateOne({ postId: id }, updateDocument);
+    const result = await db.collection(process.env.POST_COLLECTION!).updateOne({ _id: id }, updateDocument);
     res.status(200).json("Dislikes Updated" + " id:" + id);
 
     //GET brukeren sine kommentarer
