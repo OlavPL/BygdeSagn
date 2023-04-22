96cb734f4e13e355dd1df04a410200c4e2a9e43f
johlil9 <243333@usn.no>
2023-04-22 23:11:03 +0200

    Organized code and replaced input with textarea

--- frontend/components/comments.tsx
+++ frontend/components/comments.tsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
+import { useSession, signOut, getSession } from 'next-auth/react';
+import { format } from "date-fns";
 import PostComment from './postComment';
-const Comment = (props: any) => {
-  interface CommentsProps {
-    comments?: string[],
-    _id: string
-  }
+import { comment } from 'postcss';
 
+interface CommentsProps {
+  comments?: string[],
+  _id: string
+}
+
+const Comment = (props: any) => {
   const [showComment, setShowComment] = useState(false);
   const [comments, setComments] = useState<string[]>([]);
   const [newComment, setNewComment] = useState('');
@@ -29,7 +33,7 @@ const Comment = (props: any) => {
     setShowComment(!showComment);
   }
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
       setNewComment(event.target.value)
     }
     
@@ -47,7 +51,7 @@ const Comment = (props: any) => {
             >
                 {showComment ? 'Skjul kommentar' : 'Vis kommentar'}
             </button>
-
+            
             {showComment && (
                 <>
                     <PostComment
@@ -57,13 +61,15 @@ const Comment = (props: any) => {
                     />
                     <ul className="mt-4 space-y-4 text-lg">
                         {comments.map((comments, index) => (
-                            <li key={index} className="p-2 bg-gray-100 rounded shadow-md">    
+                            <li key={index} className="p-2 bg-gray-100 rounded shadow-md">  
                                 {comments}
+                                <span className="text-gray-500 mr-2">, { format(new Date(),'dd. MMMM /yy HH:MM')}</span>
                             </li>
                         ))}
                     </ul>
                 </>
             )}
+            
         </div>
       )}
 
--- frontend/components/postComment.tsx
+++ frontend/components/postComment.tsx
@@ -3,7 +3,7 @@ import React, { ChangeEvent } from 'react';
 
 interface PostCommentProps {
   userText: string;
-  handleInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleInputChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
   handleSubmit: () => void;
 }
 
@@ -14,14 +14,14 @@ const PostComment: React.FC<PostCommentProps> = ({
 }) => {
   return (
     <div className="mt-4">
-      <input
-        className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="text"
+      <textarea
+        className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline whitespace-pre-wrap break-words resize-none"
         placeholder="Skriv melding ..."
         required maxLength={200}
         value={userText}
         onChange={handleInputChange}
-      />
+        rows={3}
+    ></textarea>
       <button
         className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-2"
         onClick={handleSubmit}
@@ -29,7 +29,7 @@ const PostComment: React.FC<PostCommentProps> = ({
         Send
       </button>
     </div>
-  );
-};
+  )
+}
 
 export default PostComment;
