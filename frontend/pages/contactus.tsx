b5807b20d02dd303345fa678be2d56358e7a5d1f
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-05-09 11:43:05 +0200

    Contact us

--- frontend/pages/contactus.tsx
+++ frontend/pages/contactus.tsx
@@ -6,8 +6,8 @@ import Gravatar from 'react-gravatar';
 
 interface MemberData {
   name: string;
-  email: string;
-  phone: string;
+  hoved: string;
+  sekundær: string;
   roles: string[];
 }
 
@@ -17,20 +17,20 @@ const contactus: NextPage = () => {
   const memberData: MemberData[] = [
     {
       name: 'Herman Simonsen',
-      email: 'herman.fsi@gmail.com',
-      phone:'46779515',
+      hoved: 'herman.fsi@gmail.com',
+      sekundær:'46779515',
       roles: ['Prosjekt Leder', 'Brukerstøtte'],
     },
     {
       name: 'Olav Pålerud Lille-Østerholt',
-      email: 'olavpl.div@gmail.com',
-      phone:'48000968',
+      hoved: 'olavpl.div@gmail.com',
+      sekundær:'olavlilleosterholt@hotmail.com',
       roles: ['Kode Leder','Analyse'],
     },
     {
       name: 'John Ivar Lilleøren Hagene',
-      email: '243333@usn.no',
-      phone:'41511456',
+      hoved: '243333@usn.no',
+      sekundær:'41511456',
       roles: ['Dokumentasjons ansvarlig','HR'],
     },
   ];
@@ -43,7 +43,7 @@ const contactus: NextPage = () => {
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
       {memberData.map((member, index) => (
         <div key={index} className="mb-6 bg-white rounded-md p-4 flex items-center">
-          <Gravatar email={member.email} size={60} className="mr-4 rounded-full" />
+          <Gravatar email={member.hoved} size={60} className="mr-4 rounded-full" />
           <div>
             <h3 className="text-lg font-medium">{member.name}</h3>
             <div className="mt-2">
@@ -52,8 +52,9 @@ const contactus: NextPage = () => {
               ))}
             </div>
             <div className="mt-2 text-gray-700 text-sm">
-              <p className="mb-1">Epost: {member.email}</p>
-              <p>Telefon: {member.phone}</p>
+              <p className="mb-1">Hoved: {member.hoved}</p>
+              <p>Sekundær: {member.sekundær
+              }</p>
             </div>
           </div>
         </div>
