c0510aaa686e6ec4f1aac64acd4450db2646d686
Herman Simonsen <69625088+Focuselite1@users.noreply.github.com>
2023-05-11 02:07:51 +0200

    Commenting / Validering

--- frontend/pages/api/post/Post.ts
+++ frontend/pages/api/post/Post.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
+import validator from "validator";
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,25 +9,26 @@ export default async function handler(
 ) {
   const client = await clientPromise;
   const collection = client.db("App_Db").collection(process.env.POST_COLLECTION!);
-
+  //GET Sagn
   if (req.method === "GET") {
-    let result
-    if(!req.query._id || req.query._id == null){
+    let result;
+    if (!req.query._id || req.query._id == null) {
       result = await collection.find().toArray();
-    }
-    else {
+    } else if (validator.isMongoId(req.query._id.toString())) {
       result = await client
-      .db("App_Db")
-      .collection(process.env.POST_COLLECTION!)
-      .findOne({ _id: new ObjectId(req.query._id!.toString()) });
+        .db("App_Db")
+        .collection(process.env.POST_COLLECTION!)
+        .findOne({ _id: new ObjectId(req.query._id.toString()) });
+    } else {
+      return res.status(400).json({ error: "Invalid _id parameter" });
     }
-    
+  
     if (result === null || result.length <= 0) {
       res.status(404).json({ message: "No posts found" });
     } else {
       res.status(200).json(result);
     }
-
+    //Slett Sagn
   } else if (req.method === "DELETE") {
     const result = await collection.deleteOne({ _id: new ObjectId(req.query._id?.toString()) });
 
@@ -35,32 +37,47 @@ export default async function handler(
     } else {
       res.status(404).json({ message: "Post not found" });
     }
-
-  } else if (req.method === "POST") {
+    //Post Sagn
+  }if (req.method === 'POST') {
+    const { stedsnavn, kommune } = req.body;
+  
+    // validate stedsnavn and kommune.. Validering av Text felt og Tittel skjer i Frontend
+    if (!stedsnavn || !validator.isAlpha(stedsnavn)) {
+      return res.status(400).json({ error: 'Invalid stedsnavn' });
+    }
+  
+    if (!kommune || !kommune.fylkesnummer || !validator.isNumeric(kommune.fylkesnummer)
+        || !kommune.kommunenummer || !validator.isNumeric(kommune.kommunenummer)) {
+      return res.status(400).json({ error: 'Invalid kommune' });
+    }
+  
     try {
       const client = await clientPromise;
-      const db = client.db("App_Db");
-
+      const db = client.db('App_Db');
+  
       let myPost = await db.collection(process.env.POST_COLLECTION!).insertOne(req.body);
-      if(req.body.stedsnavn){
-
-        await db.collection("fylker").updateOne(
+  
+      if (req.body.stedsnavn) {
+        await db.collection('fylker').updateOne(
           { fylkenummer: req.body.kommune.fylkesnummer },
           {
             $addToSet: {
-               "kommuner.$[kommune].stedsnavnList":  {kommune:req.body.kommune.kommunenummer ,stedsnavn: req.body.stedsnavn}
+              'kommuner.$[kommune].stedsnavnList': {
+                kommune: req.body.kommune.kommunenummer,
+                stedsnavn: req.body.stedsnavn
+              }
             }
           },
-          { arrayFilters: [ { "kommune.kommunenummer": req.body.kommune.kommunenummer } ] }
-        )
+          { arrayFilters: [{ 'kommune.kommunenummer': req.body.kommune.kommunenummer }] }
+        );
       }
+      
       return res.status(200).json(myPost);
-
     } catch (e) {
       console.error(e);
       return res.status(301);
     }
-
+    //Update Sagn
   } else if (req.method === "PUT") {
     try {
       const client = await clientPromise;
